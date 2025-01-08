001a73bc965ad3d6a60b428e8cceb3083dad7df4
acontius <ahuram188@gmail.com>
2025-01-08 14:33:09 +0330

    creating a pull request for some random prefrences !

--- Database/createdb.sql
+++ Database/createdb.sql
@@ -170,30 +170,30 @@ CREATE TABLE compatible_gm_slot (
 
 CREATE TABLE client (
     client_id          SERIAL PRIMARY KEY, 
-    phone_number       TEXT NOT NULL UNIQUE,
+    phone_number       VARCHAR(11) NOT NULL UNIQUE,
     first_name         VARCHAR(50) NOT NULL,
     last_name          VARCHAR(50) NOT NULL,
-    wallet_balance     DECIMAL(12, 2) NOT NULL DEFAULT 0.00,
-    time_stamp         TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    referral_code      BIGINT NOT NULL UNIQUE
+    wallet_balance     DECIMAL(12, 2) NOT NULL DEFAULT 0.00, --if we want to use Rial instead of $ we shall use "BIGINT NOT NULL DEFAULT 0"
+    time_stamp         TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    referral_code      VARCHAR(16) NOT NULL UNIQUE
 );
 
 CREATE TABLE vip_client (
     client_id       INT PRIMARY KEY, 
-    expiration_time TIMESTAMP NOT NULL,
+    expiration_time TIMESTAMP WITH TIME ZONE NOT NULL, --SAME AS BEFORE 
     FOREIGN KEY (client_id) REFERENCES client (client_id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE address_of_client (
-    client_id       INT NOT NULL, 
     province        VARCHAR(20) NOT NULL,
+    client_id       INT NOT NULL, 
     remain_address  VARCHAR(255) NOT NULL,
     PRIMARY KEY (client_id, province, remain_address),
     FOREIGN KEY (client_id) REFERENCES client (client_id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE shopping_cart (
-    cart_number    SERIAL NOT NULL, 
+    cart_number    INT NOT NULL, 
     client_id      INT NOT NULL,
     cart_status    cart_status_enum NOT NULL,
     PRIMARY KEY (client_id, cart_number),
@@ -201,12 +201,12 @@ CREATE TABLE shopping_cart (
 );
 
 CREATE TABLE discount_code (
-    code            INT PRIMARY KEY, 
+    code            SERIAL PRIMARY KEY, 
     amount          DECIMAL(5, 2) CHECK (amount > 0),
     discount_limit  DECIMAL(5, 2) CHECK (discount_limit > 0),
     usage_count     SMALLINT CHECK (usage_count >= 0), 
     expiration_time TIMESTAMP,
-    code_type       discount_enum NOT NULL
+    code_type       discount_enum NOT NULL --DASTKHOSH ! but be aware that it is static and if we want to change public code we will have problem !! 
 );
 
 --I decide to delete public_code and instead add `type` in discount_code
@@ -214,7 +214,7 @@ CREATE TABLE discount_code (
 CREATE TABLE private_code (
     code        INT PRIMARY KEY, 
     client_id   INT NOT NULL,
-    time_stamp  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    time_stamp  TIMESTAMP DEFAULT CURRENT_TIMESTAMP, --SAME AS BEFORE
     FOREIGN KEY (client_id) REFERENCES client (client_id) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (code) REFERENCES discount_code (code) ON UPDATE CASCADE ON DELETE CASCADE
 );
@@ -237,6 +237,8 @@ CREATE TABLE locked_shopping_cart (
     cart_number     INT NOT NULL, 
     client_id       INT NOT NULL,
     locked_number   SERIAL NOT NULL,
+    time_stamp      TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+
     PRIMARY KEY (client_id, cart_number, locked_number),
     FOREIGN KEY (client_id) REFERENCES shopping_cart (client_id) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (cart_number) REFERENCES shopping_cart (cart_number) ON UPDATE CASCADE ON DELETE CASCADE
@@ -258,8 +260,8 @@ CREATE TABLE subscribes (
 );
 
 CREATE TABLE refers (
-    referee_id  INT PRIMARY KEY, 
-    referrer_id INT NOT NULL,
+    referee_id  VARCHAR(16) PRIMARY KEY, 
+    referrer_id VARCHAR(16) NOT NULL,
     FOREIGN KEY (referee_id) REFERENCES client (client_id) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (referrer_id) REFERENCES client (client_id) ON UPDATE CASCADE ON DELETE CASCADE
 );
@@ -281,7 +283,7 @@ CREATE TABLE applied_to (
     client_id       INT NOT NULL,
     locked_number   INT NOT NULL,
     discount_code   INT NOT NULL, 
-    time_stamp      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    time_stamp      TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (client_id, cart_number, locked_number, discount_code),
     FOREIGN KEY (discount_code) REFERENCES discount_code (code) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY (client_id, cart_number, locked_number) REFERENCES locked_shopping_cart (client_id, cart_number, locked_number) ON UPDATE CASCADE ON DELETE CASCADE
