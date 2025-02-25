c09b7ecc11e43c958f5ac98a86bffc51479b96f2
DKeshavarz <dankeshavarz1075@gmail.com>
2025-02-25 04:21:55 +0330

    Chore : Add file structre

---
+++ back/internal/storage/db.go
@@ -0,0 +1,51 @@
+package storage
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+)
+
+var dataBase *sql.DB
+func InitDB() error {
+	err := godotenv.Load()
+
+	if err != nil {
+		return err
+	}
+
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	conectionInfo := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+	dbHost, dbUser, dbPass, dbName, dbPort)
+
+	db ,err := sql.Open("postgres" , conectionInfo)
+
+	if err != nil {
+		return err
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		return err
+	}
+
+	dataBase = db
+	return nil
+}
+
+func GetDB()(*sql.DB , error){
+	if dataBase == nil {
+		return nil , errors.New("pointer is null")
+	}
+	return dataBase , nil
+}
