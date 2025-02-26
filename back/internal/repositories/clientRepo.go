a765d3c570e2d83e32613518c425a90019e3d672
DKeshavarz <dankeshavarz1075@gmail.com>
2025-02-26 19:11:44 +0330

    Chore : Retrive data from data base for client

--- back/internal/repositories/clientRepo.go
+++ back/internal/repositories/clientRepo.go
@@ -1,26 +1,91 @@
 package repositories
 
+import (
+	"database/sql"
+	"errors"
+	"time"
 
-// func Insert(client *client.ClientAbstract)(id int,err error){
-// 	//TODO:
+	"github.com/pissaze/internal/models"
+	"github.com/pissaze/internal/storage"
+)
+
+
+
+func GetClientByPhoneNumber( phoneNumber string) (models.ClientAbstract, error) {
 	
-// 	return 
-// }
+	var client models.Client
+	query := `
+		SELECT client_id, phone_number, first_name, last_name, 
+		       wallet_balance, time_stamp, referral_code
+		FROM client
+		WHERE phone_number = $1`
 
-// func Get(id int)(client *client.ClientAbstract, err error){
-// 	//TODO:
-// 	return
-// }
+	db := storage.GetDB()
 
-// func Update(client *client.ClientAbstract)(err error){
-// 	//TODO:
-// 	return
-// }
+	err := db.QueryRow( query, phoneNumber).Scan(
+		&client.ClientID, &client.PhoneNumber, &client.FirstName, 
+		&client.LastName, &client.WalletBalance, &client.Timestamp, 
+		&client.ReferralCode,
+	)
 
-// func Delete(id int)(err error){
-// 	//TODO:
-// 	return
-// }
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, errors.New("client not found")
+		}
+		return nil, errors.New("undfine error")
+	}
+
+	return client, nil
+}
+
+func GetClientAddressByClientID(ClientID int)(address []models.AddressOfClient, err error){
+	addressQuery := `
+		SELECT province, remain_address
+		FROM address_of_client
+		WHERE client_id = $1`
+
+	db := storage.GetDB()
+	rows, err := db.Query(addressQuery, ClientID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var addresses []models.AddressOfClient
+	for rows.Next() {
+		var address models.AddressOfClient
+		if err := rows.Scan(&address.Province, &address.RemainAddress); err != nil {
+			return nil, err
+		}
+		addresses = append(addresses, address)
+	}
+	return addresses, nil
+}
+
+func GetVIPClientByID(ClientID int)(client *models.VIPClient,err error){
+	
+	var expirationTime time.Time 
+	vipQuery := `
+		SELECT expiration_time
+		FROM vip_client
+		WHERE client_id = $1`
+
+	db := storage.GetDB()
+	err = db.QueryRow(vipQuery, ClientID).Scan(&expirationTime)
+
+	switch {
+	case errors.Is(err, sql.ErrNoRows):
+		return nil, nil 
+	case err != nil:
+		return nil, err
+	default:
+		return &models.VIPClient{
+			ExpirationTime: expirationTime,
+		}, nil
+	}
+}
+
 
 // ----- validators -----
 // func validate(client *client.ClientAbstract)(err error){
