a614d924728206d348b6af683e65d2f9e444b3ef
DKeshavarz <dankeshavarz1075@gmail.com>
2025-02-26 23:27:00 +0330

    Feat : Add pisaz_test to test database tables

---
+++ back/internal/storage/pisaz_test.sql
@@ -0,0 +1,17 @@
+-- SELECT * 
+-- FROM client c LEFT JOIN address_of_client a ON c.client_id = a.client_id;
+
+-- SELECT * 
+-- FROM client c NATURAL JOIN vip_client v ;
+
+-- SELECT * 
+-- FROM product p ,product_cpu c
+-- WHERE c.product_id = p.id;
+
+-- SELECT * 
+-- FROM product p ,product_hdd h
+-- WHERE h.product_id = p.id;
+
+-- SELECT * 
+-- FROM product p ,product_cooler c
+-- WHERE c.product_id = p.id;
