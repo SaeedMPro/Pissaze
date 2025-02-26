a34e704b2f5876b862f520640a75174e7d5bcf6e
DKeshavarz <dankeshavarz1075@gmail.com>
2025-02-26 14:12:28 +0330

    Chore : repos

---
+++ back/internal/repositories/client/client.go
@@ -0,0 +1,30 @@
+package client
+
+import "github.com/pissaze/internal/models/client"
+
+func Insert(client *client.ClientAbstract)(id int,err error){
+	//TODO:
+	
+	return 
+}
+
+func Get(id int)(client *client.ClientAbstract, err error){
+	//TODO:
+	return
+}
+
+func Update(client *client.ClientAbstract)(err error){
+	//TODO:
+	return
+}
+
+func Delete(id int)(err error){
+	//TODO:
+	return
+}
+
+// ----- validators -----
+// func validate(client *client.ClientAbstract)(err error){
+// 	//TODO:
+// 	return
+// }
