db7a9902ecdbbd016216790544f8859341239549
DKeshavarz <dankeshavarz1075@gmail.com>
2025-02-26 19:00:28 +0330

    Chore : Add DTO's for commucation

---
+++ back/internal/dto/api.go
@@ -0,0 +1,12 @@
+package dto
+
+type LoginRequest struct {
+	PhoneNumber string `json:"phone_number" binding:"required"`
+}
+
+type APIResponse struct {
+	Success bool        `json:"success"`
+	Message string      `json:"message,omitempty"`
+	Data    interface{} `json:"data,omitempty"`
+	Error   string      `json:"error,omitempty"`
+}
