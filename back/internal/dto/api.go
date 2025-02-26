341d8325c1446ee2e4ccf9aeede3b072c2214bac
DKeshavarz <dankeshavarz1075@gmail.com>
2025-02-27 00:16:20 +0330

    Chore : Add Product list to dto

--- back/internal/dto/api.go
+++ back/internal/dto/api.go
@@ -1,12 +1,19 @@
 package dto
 
-type LoginRequest struct {
-	PhoneNumber string `json:"phone_number" binding:"required"`
-}
+import "github.com/pissaze/internal/models"
 
 type APIResponse struct {
 	Success bool        `json:"success"`
 	Message string      `json:"message,omitempty"`
 	Data    interface{} `json:"data,omitempty"`
 	Error   string      `json:"error,omitempty"`
+}
+
+type LoginRequest struct {
+	PhoneNumber string `json:"phone_number" binding:"required"`
+}
+
+type ProductList struct {
+	ProductList []models.ProductInterface
+	Size        int
 }
