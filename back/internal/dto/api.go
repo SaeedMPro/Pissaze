694d139582fd76c82dbc04010819319f0d0321f4
DKeshavarz <dankeshavarz1075@gmail.com>
2025-02-27 04:22:16 +0330

    Refactor : Spit error and sucsess massage

--- back/internal/dto/api.go
+++ back/internal/dto/api.go
@@ -2,11 +2,15 @@ package dto
 
 import "github.com/pissaze/internal/models"
 
-type APIResponse struct {
-	Success bool        `json:"success"`
-	Message string      `json:"message,omitempty"`
-	Data    interface{} `json:"data,omitempty"`
-	Error   string      `json:"error,omitempty"`
+type ErrorResponse struct {
+    Success bool   `json:"success" example:"false"`
+    Error   string `json:"error" example:"error message"`
+}
+
+type SuccessResponse struct {
+    Success bool        `json:"success" example:"true"`
+    Message string      `json:"message,omitempty" example:"message"`
+    Data    interface{} `json:"data,omitempty"`
 }
 
 type LoginRequest struct {
