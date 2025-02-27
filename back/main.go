e717d2f9c1a12d3687790c98a7a4d1ae2079e2cf
DKeshavarz <dankeshavarz1075@gmail.com>
2025-02-27 04:25:31 +0330

    Chore : Add new Dependencie

--- back/main.go
+++ back/main.go
@@ -5,7 +5,13 @@ import (
 	_ "github.com/pissaze/internal/storage"
 )
 
-
+// @title Pisaz
+// @version 1.0
+// @description API for managing products, and users in a hardware shopping site.
+// @contact.name Saeed, Danny
+// @contact.email dan
+// @host localhost:8082
+// // @BasePath /api
 func main(){
 	server.Start()
 }
