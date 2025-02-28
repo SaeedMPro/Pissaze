96c8ff926d296996c45675044c2e483d452323b9
Farzin Hamzehi <5farzinhamzei@gmail.com>
2025-02-28 22:34:33 +0330

    Feat : Create login page

---
+++ client/src/app/login/index.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Login(){
+    return <div className='w-full max-h-screen relative bg-blue-500'>
+        <figure className='absolute top-0 left-0 bottom-0 right-0'>
+            <Image src='/images/BG.webp' alt='background' />
+        </figure>
+        <p className='font-bold'>fdfdfdf</p>
+    </div>
+}
