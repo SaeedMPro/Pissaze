5274fe87df1556735b6296c920694ab4b57c43b0
Farzin Hamzehi <5farzinhamzei@gmail.com>
2025-03-04 00:12:51 +0330

    Feat : add CompatDialog

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,10 +1,12 @@
 import {configureStore} from "@reduxjs/toolkit";
 //reducer
 import tabSlice from "@/redux/features/tabSlice";
+import modalSlice from "@/redux/features/modalSlice";
 
 export const store = configureStore({
     reducer: {
-        tabs : tabSlice
+        tabs : tabSlice,
+        modal : modalSlice
     }
 })
 
