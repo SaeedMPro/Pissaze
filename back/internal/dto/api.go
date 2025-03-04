aa803c06c7b93c5748a25245a3ef461c0421319d
DKeshavarz <dankeshavarz1075@gmail.com>
2025-03-04 12:42:09 +0330

    Chore: add login respons

--- back/internal/dto/api.go
+++ back/internal/dto/api.go
@@ -3,18 +3,23 @@ package dto
 import "github.com/pissaze/internal/models"
 
 type ErrorResponse struct {
-    Success bool   `json:"success" example:"false"`
-    Error   string `json:"error" example:"error message"`
+	Success bool   `json:"success" example:"false"`
+	Error   string `json:"error" example:"error message"`
 }
 
 type SuccessResponse struct {
-    Success bool        `json:"success" example:"true"`
-    Message string      `json:"message,omitempty" example:"message"`
-    Data    interface{} `json:"data,omitempty"`
+	Success bool        `json:"success" example:"true"`
+	Message string      `json:"message,omitempty" example:"message"`
+	Data    interface{} `json:"data,omitempty"`
 }
 
 type LoginRequest struct {
 	PhoneNumber string `json:"phone_number" binding:"required"`
+}
+
+type LoginRespons struct {
+	Token string `json:"token"`
+	IsVip bool   `json:"is_vip"`
 }
 
 type ProductList struct {
