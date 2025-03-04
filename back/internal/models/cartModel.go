b63e3c7221c1eba89144d17e8b977fe04e250610
saeedmpro <mzahry36@gmail.com>
2025-03-04 14:33:36 +0330

    Feat: add ShoppingCart and LockedShoppingCart models with interface methods

---
+++ back/internal/models/cartModel.go
@@ -0,0 +1,34 @@
+package models
+
+import "time"
+
+type CartInterface interface {
+	GetStatus()
+	GetCart()
+}
+
+type ShoppingCart struct {
+	CartNumber int
+	ClientID   int
+	CartStatus CartStatusEnum
+}
+
+type LockedShoppingCart struct {
+	ShoppingCart
+	Products         []ProductShoppingCart
+	LockedCartNumber int
+	TimeStamp        time.Time
+}
+
+type ProductShoppingCart struct {
+	Product   Product
+	Quantity  int
+	CartPrice int
+}
+
+func (s ShoppingCart) GetCart() ShoppingCart     { return s }
+func (s ShoppingCart) GetStatus() CartStatusEnum { return s.CartStatus }
+
+func (ls LockedShoppingCart) GetCart() ShoppingCart              { return ls.ShoppingCart }
+func (ls LockedShoppingCart) GetStatus() CartStatusEnum          { return ls.CartStatus }
+func (ls LockedShoppingCart) GetProducts() []ProductShoppingCart { return ls.Products }
