e67a0c9041906d338ff107613fefc937fa9e5d79
DKeshavarz <dankeshavarz1075@gmail.com>
2025-03-04 16:00:15 +0330

    Chore: add gwrCliams to jwt servise

--- back/internal/service/jwt.go
+++ back/internal/service/jwt.go
@@ -7,3 +7,8 @@ import "github.com/pissaze/internal/models"
 func GentateTokenStr(client *models.ClientAbstract)(string, error){
 	return "", nil
 }
+
+func GetClaims(token string)(*models.Claims,error){
+	return nil, nil
+}
+
