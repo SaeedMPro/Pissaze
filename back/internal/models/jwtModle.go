d96f7a856165aff0b1984557c56db969166b5e34
DKeshavarz <dankeshavarz1075@gmail.com>
2025-03-05 00:17:50 +0330

    Chore: add jwt claims

--- back/internal/models/jwtModle.go
+++ back/internal/models/jwtModle.go
@@ -3,6 +3,6 @@ package models
 import "github.com/dgrijalva/jwt-go/v4"
 
 type Claims struct {
-	Number string  `json:"phone_number" binding:"required"`
+	PhoneNumber string  `json:"phone_number" binding:"required"`
 	jwt.StandardClaims 
 }
