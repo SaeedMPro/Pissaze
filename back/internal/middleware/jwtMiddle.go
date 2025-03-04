b39b666b735e76b44a68b884c6bf10fe5ca42de4
DKeshavarz <dankeshavarz1075@gmail.com>
2025-03-05 00:19:31 +0330

    Chore: add midlewares

---
+++ back/internal/middleware/jwtMiddle.go
@@ -0,0 +1,77 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/dgrijalva/jwt-go/v4"
+	"github.com/gin-gonic/gin"
+	"github.com/pissaze/internal/dto"
+	"github.com/pissaze/internal/service"
+)
+
+
+
+func Auth() gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+		tokenString := c.GetHeader("Authorization")
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, http.ErrAbortHandler
+			}
+			return []byte(service.JwtSecretKey), nil
+		})
+		fmt.Println("here one")
+		if err != nil || !token.Valid {
+			c.JSON(http.StatusUnauthorized, dto.ErrorResponse{
+				Success: false,
+				Error: fmt.Sprintf("err is %s , and valid %b", err.Error(), token.Valid),
+			})
+			c.Abort() 
+			return
+		}
+		fmt.Println("here two")
+		if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+			c.Set("claims", claims)
+		} else {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+			c.Abort()
+			return
+		}
+		fmt.Println("here three")
+		c.Next() 
+	}
+}
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token := c.Request.Header.Get("Authorization")
+		if token == "" {
+			c.JSON(http.StatusUnauthorized, dto.ErrorResponse{
+				Success: false,
+				Error: "token not found",
+			})
+			c.Abort()
+			return
+		}
+
+		if len(token) > 6 && token[:7] == "Bearer " {
+			token = token[7:] 
+		}
+
+		phoneNumber, err := service.ExtractPhoneNumber(token)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, dto.ErrorResponse{
+				Success: false,
+				Error: fmt.Sprintf("can pars token : %s", err.Error()),
+			})
+			c.Abort()
+			return
+		}
+
+		c.Set("phone_number", phoneNumber) 
+		c.Next()
+	}
+}
