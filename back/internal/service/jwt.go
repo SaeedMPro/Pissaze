2ef1e74270c485fe7a1023ced3ccf2e145493fbc
saeedmpro <mzahry36@gmail.com>
2025-03-07 02:05:33 +0330

    Feat: load JWT secret key from environment variable

--- back/internal/service/jwt.go
+++ back/internal/service/jwt.go
@@ -2,18 +2,22 @@ package service
 
 import (
 	"errors"
+	"fmt"
+	"os"
 	"time"
 
 	"github.com/dgrijalva/jwt-go/v4"
 	"github.com/pissaze/internal/models"
 )
 
-var JwtSecretKey = []byte("your_secret_key")
+var JwtSecretKey = []byte(os.Getenv("JWT_SECRET_KEY"))
 const(
 	expireTime   = 24 * time.Hour
 ) 
 
 func GenerateTokenStr(client *models.ClientAbstract) (string, error) {
+
+    fmt.Println(JwtSecretKey)
     exp := time.Now().Add(expireTime)
     claims := models.Claims{
         PhoneNumber: (*client).GetClient().PhoneNumber,
