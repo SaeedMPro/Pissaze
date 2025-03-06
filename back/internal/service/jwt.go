f5c56aee70e162242b915260946b99ef7d546127
saeedmpro <mzahry36@gmail.com>
2025-03-07 01:56:40 +0330

    Feat: enhance JWT middleware to include VIP status in claims extraction

--- back/internal/service/jwt.go
+++ back/internal/service/jwt.go
@@ -13,40 +13,39 @@ const(
 	expireTime   = 24 * time.Hour
 ) 
 
-
 func GenerateTokenStr(client *models.ClientAbstract) (string, error) {
-	exp := time.Now().Add(expireTime)
-	claims := models.Claims{
-		PhoneNumber: (*client).GetClient().PhoneNumber,
-		StandardClaims: jwt.StandardClaims{
-			ExpiresAt : &jwt.Time{Time: exp}, 
-		},
-	}
-	
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+    exp := time.Now().Add(expireTime)
+    claims := models.Claims{
+        PhoneNumber: (*client).GetClient().PhoneNumber,
+        IsVIP:       (*client).IsVIP(), 
+        StandardClaims: jwt.StandardClaims{
+            ExpiresAt: &jwt.Time{Time: exp},
+        },
+    }
+    
+    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+    
+    tokenString, err := token.SignedString(JwtSecretKey)
+    if err != nil {
+        return "", err
+    }
 
-	tokenString, err := token.SignedString(JwtSecretKey)
-	if err != nil {
-		return "", err
-	}
-
-	return tokenString, nil
+    return tokenString, nil
 }
 
-func ExtractPhoneNumber(tokenString string) (string, error) {
-	token, err := jwt.ParseWithClaims(tokenString, &models.Claims{}, func(token *jwt.Token) (interface{}, error) {
-		return JwtSecretKey, nil
-	})
-	if err != nil {
-		return "", err
-	}
-
-	if claims, ok := token.Claims.(*models.Claims); ok && token.Valid {
-		return claims.PhoneNumber, nil
-	}
-
-	return "", errors.New("invalid token")
+func ExtractClaims(tokenString string) (*models.Claims, error) {
+    claims := &models.Claims{}
+    token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+        return JwtSecretKey, nil
+    })
+    
+    if err != nil {
+        return nil, err
+    }
+    
+    if !token.Valid {
+        return nil, errors.New("invalid token")
+    }
+    
+    return claims, nil
 }
-
-
-
